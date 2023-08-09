76290ac54fb4f524572b5f274cf6a9141f474bc4
emrulla muhter <emrullauigur@gmai.com>
2023-08-09 15:40:52 +0300

    Spring 01

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -0,0 +1,50 @@
+package com.cydeo.bean_annotation;
+
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.casefactory.DellCase;
+import com.cydeo.bean_annotation.config.ComputerConfigration;
+import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.module.Configuration;
+
+public class  ComputerTest {
+    public static void main(String[] args) {
+
+        //creating container by using application context
+        ApplicationContext container=new AnnotationConfigApplicationContext(ComputerConfigration.class);
+
+        //creating container by using BeanFactory
+        BeanFactory container2=new AnnotationConfigApplicationContext();
+
+
+        SonyMonitor sony=container.getBean(SonyMonitor.class);
+        AsusMotherboard asus=container.getBean(AsusMotherboard.class);
+        DellCase dell =container.getBean(DellCase.class);
+
+        PC newpc =new PC(dell,sony,asus);
+        newpc.powerUp();
+
+        System.out.println("----------------------------------------");
+        Monitor theMonitor=container.getBean(Monitor.class);
+        Motherboard theMotherboard=container.getBean(Motherboard.class);
+        Case theCase =container.getBean(Case.class);
+
+        PC  Secondpc =new PC(theCase,theMonitor,theMotherboard);
+        newpc.powerUp();
+
+        System.out.println("----------Multiple Objects-------------");
+
+    Monitor theMonitor2=container.getBean("AcerMonitor",Monitor.class);//Default bean name
+Monitor theasusmonitor=container.getBean("asus",Monitor.class);//CUSTOM BEAN NAME
+        Monitor theasusmonitor3=container.getBean(Monitor.class);//@Primary
+
+
+    }
+}
---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfigration.java
@@ -0,0 +1,35 @@
+package com.cydeo.bean_annotation.config;
+
+
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.casefactory.DellCase;
+import com.cydeo.bean_annotation.monitorfactory.AcerMonitor;
+import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ComputerConfigration {
+    @Bean(name = "sony")
+public Monitor SonyMonitor(){
+    return new SonyMonitor("12x3","213ed",23);
+
+}
+@Bean(name = "asus")
+@Primary  // make default
+public Monitor AcerMonitor(){
+        return new AcerMonitor("32ds","dw3q",32);
+}
+@Bean
+    public Case DellCase(){
+        return new DellCase("mew2","wd22","21s");
+    }
+@Bean
+    public Motherboard AsusMotherBoard(){
+        return new AsusMotherboard("23CS","D3D3",12,32,"CE3");
+    }
+}
