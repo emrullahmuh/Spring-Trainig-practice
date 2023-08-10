87646d6db9c46556eb77f712a05a6fbc3c334451
emrulla muhter <emrullauigur@gmai.com>
2023-08-10 14:28:34 +0300

    Spring framework

--- Spring-06-Core-BeanScorp/src/main/java/org/cydeo/CommentApp.java
+++ Spring-06-Core-BeanScorp/src/main/java/org/cydeo/CommentApp.java
@@ -2,7 +2,7 @@ package org.cydeo;
 
 import org.cydeo.Config.comConfig;
 import org.cydeo.Module.Comment;
-import org.cydeo.Service.CommentService;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -14,9 +14,9 @@ comment.setName("john");
 comment.setText("spring test");
 
         ApplicationContext container=new AnnotationConfigApplicationContext(comConfig.class);
-
+/*
  CommentService commentService=container.getBean(CommentService.class);
 commentService.publishComment(comment);
-
+*/
     }
 }
